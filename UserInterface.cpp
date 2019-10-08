2dce83f30511f567b6fb2c314af64b32966b83b6
VadosLight <it-vadim@inbox.ru>
2019-10-09 01:01:22 +0300

    instruction -  N button

--- UserInterface.cpp
+++ UserInterface.cpp
@@ -9,6 +9,7 @@ void UserInterface::showHelp()
 	std::cout << "\tPress on rectan gle to change colour" << std::endl;
 	std::cout << "\t<- and -> change speed" << std::endl;
 	std::cout << "\tPress C to clear screen" << std::endl;
+	std::cout << "\tPress N to go to next step (while pause)" << std::endl;
 	std::cout << "==================================================" << std::endl;
 	std::cout << "==================================================" << std::endl;
 }
